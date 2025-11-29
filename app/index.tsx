5728297d4c4b6479b7a3d49b24f4b4ec300b551a
Thexthnksdnm <169040134+Thexthnksdnm@users.noreply.github.com>
2025-11-29 03:53:58 -0600

    Commit

--- app/index.tsx
+++ app/index.tsx
@@ -1,8 +1,10 @@
-import {FlatList, Pressable, Text, View } from "react-native";
+import {FlatList, Pressable, Text, View ,Image} from "react-native";
 import "./global.css"
 import { SafeAreaView } from "react-native-safe-area-context";
 import { offers } from "@/constants";
 import { Fragment } from "react";
+import cn from 'clsx';
+
 
  
 export default function index () {
@@ -11,11 +13,24 @@ export default function index () {
         <FlatList
           data={offers}
          renderItem={({ item, index }) => {
+           const isEven: boolean = index % 2 === 0;
            return (
              <View>
-               <Pressable className="offer-card" style= {{ backgroundColor: item.color}}>
+               <Pressable className={cn("offer-card", isEven ? 'flex-row-reverse' : 'flex-row')}
+                 style={{ backgroundColor: item.color }}
+               >
                  {({ pressed }) => (
                    <Fragment> 
+                        <View className={"h-full w-1/2"}>
+                        < Image source={item.image} className={"size-full"} resizeMode={"contain"}/>
+                        </View>
+                     
+                     <View className={"offer-card__info"}>
+                       <Text>
+                         {item.title}
+                       </Text>
+                     </View>
+                     
                      
                   </Fragment>
                  )}
