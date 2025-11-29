40969cf80fe8d84afb5a55c41c0747d3d886f3b6
Thexthnksdnm <169040134+Thexthnksdnm@users.noreply.github.com>
2025-11-29 04:46:25 -0600

    Commit

--- app/index.tsx
+++ app/index.tsx
@@ -1,7 +1,7 @@
-import {FlatList, Pressable, Text, View ,Image} from "react-native";
+import {FlatList, Pressable, Text, View ,Image,TouchableOpacity, ScrollView} from "react-native";
 import "./global.css"
 import { SafeAreaView } from "react-native-safe-area-context";
-import { offers } from "@/constants";
+import { images,offers } from "@/constants";
 import { Fragment } from "react";
 import cn from 'clsx';
 
@@ -9,7 +9,7 @@ import cn from 'clsx';
  
 export default function index () {
    return (
-      <SafeAreaView >
+     <SafeAreaView className="flex-1 bg-white">
         <FlatList
           data={offers}
          renderItem={({ item, index }) => {
@@ -18,6 +18,7 @@ export default function index () {
              <View>
                <Pressable className={cn("offer-card", isEven ? 'flex-row-reverse' : 'flex-row')}
                  style={{ backgroundColor: item.color }}
+                 android_ripple={{ color:"#ffffff22"}}
                >
                  {({ pressed }) => (
                    <Fragment> 
@@ -25,10 +26,16 @@ export default function index () {
                         < Image source={item.image} className={"size-full"} resizeMode={"contain"}/>
                         </View>
                      
-                     <View className={"offer-card__info"}>
-                       <Text>
+                     <View className={cn ("offer-card__info",isEven ? 'pl-10':'pr-10')}>
+                       <Text className="h1-bold text-white leading-tight">
                          {item.title}
                        </Text>
+                       <Image source={images.arrowRight}
+                         className="size-10"
+                         resizeMode="contain"
+                         tintColor="#FFFFFF"
+
+                       />
                      </View>
                      
                      
@@ -40,6 +47,20 @@ export default function index () {
            )
 
          }}
+         contentContainerClassName="pb-28 px-5"
+         ListHeaderComponent={() => (
+            <View className="flex-between flex-row w-full my-5 ">
+         <View className="flex-start">
+           <Text className="small-bold text-primary">DELIVER TO</Text>
+           <TouchableOpacity className="flex-center flex-row gap-x-1 mt-0.5">
+             <Text className="paragraph-bold text-dark-100">Nigeria</Text>
+             <Image source={images.arrowDown} className="size-3" resizeMode="contain" />
+             </TouchableOpacity>
+         </View>
+         <Text>Cart</Text>
+       </View>
+           
+         )}
         
       /> 
 
