3a29856ed2b8fea8edaf4ce561d5940cd23abc6d
Thexthnksdnm <169040134+Thexthnksdnm@users.noreply.github.com>
2025-11-29 11:18:39 -0600

    Commit

---
+++ app/(auth)/_layout.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native'
+import { SafeAreaView } from "react-native-safe-area-context";
+import { Slot } from 'expo-router';
+export default function _Layout() {
+  return (
+    <SafeAreaView>
+          <Text>Auth Layout</Text>
+          <Slot/>
+    </SafeAreaView>
+  )
+}
